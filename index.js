bc1542819ca27317fe4b858e1c8bb4ddce8bf9fc
MirasUddin <mirasuddin978@gmail.com>
2023-05-31 18:22:56 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -30,6 +31,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    
+
+    const usersCollection = client.db("bistroDb").collection("users")
+    const menuCollection = client.db("bistroDb").collection("menu")
+    const reviewCollection = client.db("bistroDb").collection("reviews")
+    const cartCollection = client.db("bistroDb").collection("carts")
+
+    app.post('/jwt', (req, res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '2h'})
+      res.send({token})
+    })
 
 
     // users related apis
@@ -65,11 +78,6 @@ async function run() {
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result)
     })
-
-    const usersCollection = client.db("bistroDb").collection("users")
-    const menuCollection = client.db("bistroDb").collection("menu")
-    const reviewCollection = client.db("bistroDb").collection("reviews")
-    const cartCollection = client.db("bistroDb").collection("carts")
 
     // menu related api
     app.get('/menu', async (req, res) => {
